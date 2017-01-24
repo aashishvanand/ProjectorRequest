b9e2b521ea2d1b0171316416bcce6165008d6cf7
Aashish Vivekanand <aashishvanand@gmail.com>
2017-01-24 10:43:43 +0530

    Added Square View

--- app/src/main/java/com/aashish/projectorrequest/Login.java
+++ app/src/main/java/com/aashish/projectorrequest/Login.java
@@ -10,13 +10,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.HashMap;
 import java.util.Map;
 
