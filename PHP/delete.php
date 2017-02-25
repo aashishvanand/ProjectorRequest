3fe10751808f1fde85e0d5a55df9df448f1cde97
Aashish Vivekanand <aashishvanand@users.noreply.github.com>
2017-02-25 22:20:30 +0530

    Update delete.php

--- PHP/delete.php
+++ PHP/delete.php
@@ -32,58 +32,75 @@
    	$code = $_SESSION['username'];
    	$projector = $_POST['projector'];
    	$dept = $_SESSION['dept'];
+		
+		$date_arr = explode(" ", $date);
+	switch(strtolower($date_arr[1])){
+	case 'january': $date_arr[1]='01';break;
+	case 'february': $date_arr[1]='02';break;
+	case 'march': $date_arr[1]='03';break;
+	case 'april': $date_arr[1]='04';break;
+	case 'may': $date_arr[1]='05';break;
+	case 'june': $date_arr[1]='06';break;
+	case 'july': $date_arr[1]='07';break;
+	case 'august': $date_arr[1]='08';break;
+	case 'september': $date_arr[1]='09';break;
+	case 'october': $date_arr[1]='10';break;
+	case 'november': $date_arr[1]='11';break;
+	case 'december': $date_arr[1]='12';break;
+	}
+	$final_date=implode('-',$date_arr);
    
    	switch ($dept) {
       	case "aero":
-   		$sql = "DELETE FROM `aero.projector` WHERE date = '$date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
+   		$sql = "DELETE FROM `aero.projector` WHERE date = '$final_date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
            	break;
        	case "auto":
-   		$sql = "DELETE FROM `auto.projector` WHERE date = '$date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
+   		$sql = "DELETE FROM `auto.projector` WHERE date = '$final_date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
            	break;
 		case "btech":
-   		$sql = "DELETE FROM `btech.projector` WHERE date = '$date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
+   		$sql = "DELETE FROM `btech.projector` WHERE date = '$final_date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
            	break;
 		case "bmed":
-   		$sql = "DELETE FROM `bmed.projector` WHERE date = '$date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
+   		$sql = "DELETE FROM `bmed.projector` WHERE date = '$final_date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
            	break;
 		case "chem":
-   		$sql = "DELETE FROM `chem.projector` WHERE date = '$date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
+   		$sql = "DELETE FROM `chem.projector` WHERE date = '$final_date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
            	break;
 		case "civil":
-   		$sql = "DELETE FROM `civil.projector` WHERE date = '$date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
+   		$sql = "DELETE FROM `civil.projector` WHERE date = '$final_date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
            	break;			
-       	case "cse":
-   		$sql = "DELETE FROM `cse.projector` WHERE date = '$date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
+      	 	case "cse":
+   		$sql = "DELETE FROM `cse.projector` WHERE date = '$finaldate' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
            	break;
 		case "eee":
-   		$sql = "DELETE FROM `eee.projector` WHERE date = '$date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
+   		$sql = "DELETE FROM `eee.projector` WHERE date = '$final_date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
            	break;
 		case "ece":
-   		$sql = "DELETE FROM `ece.projector` WHERE date = '$date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
+   		$sql = "DELETE FROM `ece.projector` WHERE date = '$final_date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
            	break;
 		case "it":
-   		$sql = "DELETE FROM `it.projector` WHERE date = '$date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
+   		$sql = "DELETE FROM `it.projector` WHERE date = '$final_date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
            	break;
 		case "mech":
-   		$sql = "DELETE FROM `mech.projector` WHERE date = '$date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
+   		$sql = "DELETE FROM `mech.projector` WHERE date = '$final_date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
            	break;
 		case "mtrcs":
-   		$sql = "DELETE FROM `mtrcs.projector` WHERE date = '$date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
+   		$sql = "DELETE FROM `mtrcs.projector` WHERE date = '$final_date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
            	break;
 		case "hd":
-   		$sql = "DELETE FROM `hd.projector` WHERE date = '$date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
+   		$sql = "DELETE FROM `hd.projector` WHERE date = '$final_date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
            	break;
 		case "pe":
-   		$sql = "DELETE FROM `pe.projector` WHERE date = '$date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
+   		$sql = "DELETE FROM `pe.projector` WHERE date = '$final_date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
            	break;
 		case "edc":
-   		$sql = "DELETE FROM `edc.projector` WHERE date = '$date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
+   		$sql = "DELETE FROM `edc.projector` WHERE date = '$final_date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
            	break;
 		case "mba":
-   		$sql = "DELETE FROM `mba.projector` WHERE date = '$date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
+   		$sql = "DELETE FROM `mba.projector` WHERE date = '$final_date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
            	break;
 		case "mca":
-   		$sql = "DELETE FROM `mca.projector` WHERE date = '$date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
+   		$sql = "DELETE FROM `mca.projector` WHERE date = '$final_date' AND hour = '$hour' AND staffcode ='$code' AND projector ='$projector'";
            	break;
    	}
    
