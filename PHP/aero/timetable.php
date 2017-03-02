70593eebc010612331a7f83bbbbb799c5dce3226
Aashish Vivekanand <aashishvanand@users.noreply.github.com>
2017-03-02 14:02:24 +0530

    Update timetable.php

--- PHP/aero/timetable.php
+++ PHP/aero/timetable.php
@@ -18,7 +18,7 @@
       $servername = "localhost";
       $username = "root";
       $password = "*******";
-      $dbname = "projector";
+      $dbname = "rec";
       
       // Create connection
       $conn = mysqli_connect($servername, $username, $password, $dbname);
